253b62174987ab2e30d5264af7a8e00966a83fe2
EmanueleBlini <emanuele.blini04@gmail.com>
2024-03-25 15:16:10 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App {
+
+    static int portNumber = 1234;
+
+    public static void main( String[] args ) {
+
+    }
+}
